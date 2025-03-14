9ebc6b936cb05d5de577fadf108a4f91b0544225
Rafael Palomar <rafael.palomar@ous-research.no>
2025-03-14 14:38:50 +0100

    ENH: Add ExternalProject_Add_FetchMethod macro

--- CMake/ExternalProjectAddFetchMethod.cmake
+++ CMake/ExternalProjectAddFetchMethod.cmake
@@ -1,19 +1,17 @@
 macro(ExternalProject_Add_FetchMethod)
-  set(options CAN_BE_OVERRIDEN)
+  set(options CAN_BE_OVERRIDDEN)
   set(oneValueArgs
     PROJECT        #Mandatory
+    DOWNLOAD_DIR   #Optional
     SOURCE_DIR     #Optional
     GIT_REPOSITORY #Optional
     GIT_TAG        #Optional
     ARCHIVE        #Optional
     ARCHIVE_MD5    #Optional
   )
+  set(multiValueArgs)
 
-  cmake_parse_arguments(_fm
-    "${options}"
-    "${oneValueArgs}"
-    ${ARGN}
-  )
+  cmake_parse_arguments(_fm "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
   #-----------------
   # Sanity checks
@@ -25,117 +23,113 @@ macro(ExternalProject_Add_FetchMethod)
   endif()
 
   # Check at least there is a fetch method
-  if(NOT _fm_SOURCE_DIR and
-      NOT _fm_GIT_REPOSITORY and
-      NOT _fm_ARCHIVE)
+  if((NOT _fm_SOURCE_DIR) AND (NOT _fm_GIT_REPOSITORY) AND (NOT _fm_ARCHIVE))
     message(FATAL_ERROR "At least one of the following parameters need to be specified: SOURCE_DIR, GIT_REPOSITORY or ARCHIVE!")
   endif()
 
   # Check mutually exclusive fetch metods
-  if(_fm_GIT_REPOSITORY and _fm_ARCHIVE)
-    message(FATAL_ERROR "GIT_REPOSITORY and ARCHIVE are mutually exclusive! Please set only one of these parameters")
+  if((_fm_GIT_REPOSITORY AND _fm_ARCHIVE) OR
+      (_fm_GIT_REPOSITORY AND _fm_SOURCE_DIR) OR
+      (_fm_ARCHIVE AND _fm_SOURCE_DIR))
+    message(FATAL_ERROR "GIT_REPOSITORY, ARCHIVE and SOURCE_DIR are mutually exclusive! Please set only one of these parameters")
   endif()
 
-  #Create and initialize ${PROJECT}_FETCH_METHOD
-  set(${PROJECT}_FETCH_METHOD "" PARENT_SCOPE)
+  # Check mutually exclusive fetch metods defined externally
+  if((${_fm_PROJECT}GIT_REPOSITORY AND ${_fm_PROJECT}ARCHIVE) OR
+      (${_fm_PROJECT}GIT_REPOSITORY AND ${_fm_PROJECT}SOURCE_DIR) OR
+      (${_fm_PROJECT}ARCHIVE AND ${_fm_PROJECT}SOURCE_DIR))
+    message(FATAL_ERROR "GIT_REPOSITORY, ARCHIVE and SOURCE_DIR external definitions for ${_fm_PROJECT} are mutually exclusive! Please set only one of them")
+  endif()
 
+  #Create and initialize ${_fm_PROJECT}_FETCH_METHOD
+  set(${_fm_PROJECT}_FETCH_METHOD)
+  set(${_fm_PROJECT}_FETCH_METHOD)
 
-  #-----------------
-  # CAN_BE_OVERRIDEN
-  #-----------------
-  # The specified method is just a default that can be overriden
-  # by setting the corresponding ${PROJECT}_<GIT_REPOSITORY|ARCHIVE|SOURCE_DIR>[_<GIT_TAG|ARCHIVE_MD5>](e.g., externally).
-  if(_fm_CAN_BE_OVERRIDEN)
+  #-----------------------------
+  #Set the default values
 
-    # GIT is default
-    if(_fm_GIT_REPOSITORY)
-
-      #Check whether there exists an archive to override
-      if(${PROJECT}_ARCHIVE)
-
-        if(${PROJECT}_ARCHIVE_MD5)
-          message(STATUS "Setting fetch method to archive: ${${PROJECT}_ARCHIVE} with MD5 ${${PROJECT}_ARCHIVE_MD5}")
-
-          list(APPEND ${PROJECT}_FETCH_METHOD
-            URL "${${PROJECT}_ARCHIVE}"
-            URL_MD5 "${${PROJECT}_ARCHIVE_MD5}"
-          )
-        else()
-          message(FATAL_ERROR "${PROJECT}_ARCHIVE overriding option detected, but no ${PROJECT}_ARCHIVE_MD5 specified!")
-        endif()
-      else()
-        message(STATUS "Setting fetch method to default (git): ${_fm_GIT_REPOSITORY} with TAG ${_fm_GIT_TAG}")
-
-        list(APPEND ${PROJECT}_FETCH_METHOD
-          GIT_REPOSITORY "${_dm_GIT_REPOSITORY}"
-          GIT_TAG "${_fm_GIT_TAG}"
-        )
-      endif()
-    endif()
-    # GIT is default
-    if(_fm_ARCHIVE)
-
-      #Check if there exist an archive to override
-      if(${PROJECT}_GIT_REPOSITORY)
-
-        if(${PROJECT}_GIT_TAG)
-          message(STATUS "Setting fetch method to git: ${${PROJECT}_GIT_REPOSITORY} with TAG ${${PROJECT}_GIT_TAG}")
-
-          list(APPEND ${PROJECT}_FETCH_METHOD
-            GIT_REPOSITORY "${${PROJECT}_GIT_REPOSITORY}"
-            GIT_TAG "${${PROJECT}_GIT_TAG}"
-          )
-        else()
-          message(FATAL_ERROR "${PROJECT}_GIT_REPOSITORY overriding option detected, but no ${PROJECT}_GIT_TAG specified!")
-        endif()
-      else()
-        message(STATUS "Setting fetch method to default (archive): ${_fm_ARCHIVE} with MD5 ${_fm_ARCHIVE_MD5}")
-
-        list(APPEND ${PROJECT}_FETCH_METHOD
-          ARCHIVE "${_dm_ARCHIVE}"
-          ARCHIVE_MD5 "${_fm_ARCHIVE_MD5}"
-        )
-      endif()
-
-      #Check whether there exists a source to override
-      if(${PROJECT}_SOURCE_DIR)
-        message(STATUS "Setting source ${PROJECT}_SOURCE_DIR to ${${PROJECT}_SOURCE_DIR}")
-
-        list(APPEND ${PROJECT}_FETCH_METHOD
-          SOURCE_DIR "${${PROJECT}_SOURCE_DIR}"
-        )
-      elseif(_fm_SOURCE_DIR)
-        message(STATUS "Setting source ${PROJECT}_SOURCE_DIR to ${_fm_SOURCE_DIR}")
-
-        list(APPEND ${PROJECT}_FETCH_METHOD
-          SOURCE_DIR "${_fm_SOURCE_DIR}"
-        )
-      endif()
-    endif()
-
-  else() #NOT CAN_BE_OVERRIDEN
-
-
-    # GIT is default
-    if(_fm_ARCHIVE)
-
-      message(STATUS "Setting fetch method to arhive: ${_fm_ARCHIVE} with MD5 ${_fm_ARCHIVE_MD5}")
-
-      list(APPEND ${PROJECT}_FETCH_METHOD
-        ARCHIVE "${_dm_ARCHIVE}"
-        ARCHIVE_MD5 "${_fm_ARCHIVE_MD5}"
+  # Check for SOURCE_DIR
+  if(_fm_SOURCE_DIR)
+    message(STATUS "Setting fetch method to SOURCE_DIR: ${_fm_SOURCE_DIR}")
+    list(APPEND _fm_FETCH_METHOD
+      SOURCE_DIR ${_fm_SOURCE_DIR}
+    )
+    # Check for ARCHIVE
+  elseif(_fm_ARCHIVE)
+    if(_fm_ARCHIVE_MD5)
+      message(STATUS "Setting fetch method to archive: ${_fm_ARCHIVE} with MD5 ${_fm_ARCHIVE_MD5}")
+      list(APPEND _fm_FETCH_METHOD
+        URL "${_fm_ARCHIVE}"
+        URL_MD5 "${_fm_ARCHIVE_MD5}"
       )
+    else()
+      message(FATAL_ERROR "_fm_ARCHIVE overriding option detected, but no _fm_ARCHIVE_MD5 specified!")
+    endif()
+    # Check for GIT_REPOSITORY
+  elseif(_fm_GIT_REPOSITORY)
+    if(_fm_GIT_TAG)
+      message(STATUS "Setting fetch method to git: ${_fm_GIT_REPOSITORY} with TAG ${_fm_GIT_TAG}")
+      list(APPEND _fm_FETCH_METHOD
+        GIT_REPOSITORY "${_fm_GIT_REPOSITORY}"
+        GIT_TAG "${_fm_GIT_TAG}"
+      )
+    else()
+      message(FATAL_ERROR "_fm_GIT_REPOSITORY overriding option detected, but no _fm_GIT_TAG specified!")
+    endif()
+  endif()
+
+  if(_fm_DOWNLOAD_DIR AND (NOT _fm_SOURCE_DIR))
+    message(STATUS "Setting download directory for ${_fm_PROJECT} to ${_fm_PROJECT}_DOWNLOAD_DIR}")
+    list(APPEND _FM_FETCH_METHOD
+      SOURCE_DIR "${_fm_DOWNLOAD_DIR}"
+    )
+  endif()
+
+  #--------------------------------------
+  # Check and override values, if needed
+
+  if(_fm_CAN_BE_OVERRIDDEN)
+
+    # Check if there is a potential override and reset the _FETCH_METHODS variable
+    if(${_fm_PROJECT}_SOURCE_DIR OR ${_fm_PROJECT}_ARCHIVE OR ${_fm_PROJECT}_GIT)
+      set(${_fm_PROJECT}_FETCH_METHOD)
     endif()
 
+    # Check for external SOURCE_DIR
+    if(${_fm_PROJECT}_SOURCE_DIR)
+      message(STATUS "Setting fetch method to SOURCE_DIR: ${${_fm_PROJECT}_SOURCE_DIR}")
+      list(APPEND ${_fm_PROJECT}_FETCH_METHOD
+        SOURCE_DIR ${${_fm_PROJECT}_SOURCE_DIR}
+      )
+      # Check for external ARCHIVE
+    elseif(${_fm_PROJECT}_ARCHIVE)
+      if(${_fm_PROJECT}_ARCHIVE_MD5)
+        message(STATUS "Setting fetch method to archive: ${${_fm_PROJECT}_ARCHIVE} with MD5 ${${_fm_PROJECT}_ARCHIVE_MD5}")
 
-    # GIT is default
-    if(_fm_GIT_REPOSITORY)
+        list(APPEND ${_fm_PROJECT}_FETCH_METHOD
+          URL "${${_fm_PROJECT}_ARCHIVE}"
+          URL_MD5 "${${_fm_PROJECT}_ARCHIVE_MD5}"
+        )
+      else()
+        message(FATAL_ERROR "${_fm_PROJECT}_ARCHIVE overriding option detected, but no ${_fm_PROJECT}_ARCHIVE_MD5 specified!")
+      endif()
+      # Check for external _GIT_REPOSITORY
+    elseif(${_fm_PROJECT}_GIT_REPOSITORY)
+      if(${_fm_PROJECT}_GIT_TAG)
+        message(STATUS "Setting fetch method to git: ${${_fm_PROJECT}_GIT_REPOSITORY} with TAG ${${_fm_PROJECT}_GIT_TAG}")
 
-      message(STATUS "Setting fetch method to arhive: ${_fm_GIT_REPOSITORY} with MD5 ${_fm_GIT_TAG}")
-
-      list(APPEND ${PROJECT}_FETCH_METHOD
-        GIT_REPOSITORY "${_dm_GIT_REPOSITORY}"
-        GIT_TAG "${_fm_GIT_TAG}"
+        list(APPEND ${_fm_PROJECT}_FETCH_METHOD
+          GIT_REPOSITORY "${${_fm_PROJECT}_GIT_REPOSITORY}"
+          GIT_TAG "${${_fm_PROJECT}_GIT_TAG}"
+        )
+      else()
+        message(FATAL_ERROR "${_fm_PROJECT}_GIT_REPOSITORY overriding option detected, but no ${_fm_PROJECT}_GIT_TAG specified!")
+      endif()
+    endif()
+    if(${_fm_PROJECT}_DOWNLOAD_DIR AND (NOT ${_fm_PROJECT}_SOURCE_DIR))
+      message(STATUS "Setting download directory for ${_fm_PROJECT}: ${${_fm_PROJECT}_DOWNLOAD_PROJECT}")
+      list(APPEND ${_fm_PROJECT}_FETCH_METHOD
+        SOURCE_DIR"${{_dm_PROJECT}_DOWNLOAD_DIR}"
       )
     endif()
   endif()
